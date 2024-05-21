b74e1c16e8ff495cd48623b109d790178144cabe
XingFenD <z2757341301@gmail.com>
2024-05-21 20:15:25 +0800

    navigator animation and icons

--- js/mainpage.js
+++ js/mainpage.js
@@ -2,6 +2,9 @@ var nav_btns = document.querySelectorAll(".nav-btn");
 
 // add click event
 for (var i = 0;i < nav_btns.length; i++) {
+    // set element id
+    nav_btns[i].setAttribute("id", `nav${i}`);
+
     // click to switch active element
     nav_btns[i].addEventListener("click", function() {
         var nav_btns = document.querySelectorAll('a[type="active"]');
@@ -11,7 +14,7 @@ for (var i = 0;i < nav_btns.length; i++) {
         this.setAttribute("type", "active");
     })
 
-    //
+    // rend the page
     nav_btns[i].addEventListener("click", function() {
         console.log('Click event triggered, starting render the content');
         // remove the former content
@@ -19,6 +22,14 @@ for (var i = 0;i < nav_btns.length; i++) {
         content.innerHTML = '';
         rending[Number(this.getAttribute("id").substr(3))](content);
     })
+
+    // move slider
+    nav_btns[i].addEventListener("click", function() {
+        var slider = document.querySelector('.slider');
+        var nav_btns = document.querySelectorAll(".nav-btn");
+        slider.setAttribute("style", `top:-${41 * (nav_btns.length - Number(this.getAttribute("id").substr(3)))}px`);
+    })
+
 }
 
 // call the event of mainpage
--- js/mainpage_rend.js
+++ js/mainpage_rend.js
@@ -1,11 +1,13 @@
 const rending = [
-    function (content) {
-        console.log("rending the nav0 pag");
-        var uname_auth = request_uname_auth();
-        var para1 = document.createElement("h1");
-        para1.setAttribute("id", "hello");
+    async function (content) { // 0首页
+        console.log("rending the nav0 page");
+        
+        var uname_auth = await request_uname_auth();
+        var para1 = document.createElement("div");
+        para1.setAttribute("class", "text title");
         var text1 = document.createTextNode(`你好`);
-        var para2 = document.createElement("p");
+        var para2 = document.createElement("div");
+        para2.setAttribute("class", "text para");
         var text2 = document.createTextNode(`${uname_auth['username']}，你的权限是${uname_auth['authority']}`);
 
         para1.appendChild(text1);
@@ -15,23 +17,26 @@ const rending = [
         // content.innerHTML = `<p>你好，用户${uname_auth['username']}</p>\n`
             // + `<p>你的权限是${uname_auth['authority']}</p>`;
     },
-    function (content) {
-        console.log("rending the nav1 pag");
+    async function (content) { // 1详情
+        console.log("rending the nav1 page");
     },
-    function (content) {
-        console.log("rending the nav2 pag");
+    async function (content) { // 2个人中心
+        console.log("rending the nav2 page");
     },
-    function (content) {
-        console.log("rending the nav3 pag");
+    async function (content) { // 3管理员页面
+        console.log("rending the nav3 page");
+    },
+    async function (content) { // 4关于
+        console.log("rending the nav4 page");
     }
 ]
 
-function request_uname_auth() {
+async function request_uname_auth() {
     var rt;
-    $.ajax({
+    await $.ajax({
         type: "GET",
         url: "../php/uname_auth_backend.php", // the corresponding back-end script
-        async: false,
+        async: true,
         success: function(res) {  // if get the return successfully
             console.log(res);
             rt = res;
