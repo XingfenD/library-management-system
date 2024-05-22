bde7494f832f173618726643b9e385aa18f39fe1
XingFenD <z2757341301@gmail.com>
2024-05-22 08:48:17 +0800

    hori-bar & veritical bar icon finish

--- js/mainpage_rend.js
+++ js/mainpage_rend.js
@@ -5,10 +5,10 @@ const rending = [
         rend_hori_bar(["1", "2", "3", "4", "1", "2", "3", "4"], content, uname_auth);
         var box = document.createElement("div");
         box.setAttribute("class", "box");
-        var para1 = document.createElement("div");
+        var para1 = document.createElement("a");
         para1.setAttribute("class", "text title");
         var text1 = document.createTextNode(`你好`);
-        var para2 = document.createElement("div");
+        var para2 = document.createElement("a");
         para2.setAttribute("class", "text para");
         var text2 = document.createTextNode(`${uname_auth['username']}，你的权限是${uname_auth['authority']}`);
 
@@ -89,12 +89,12 @@ function rend_hori_bar(btn_str_lst, content, uname_auth) {
 
     // build connection
     btn_out.appendChild(out_text);
-    h_user.appendChild(user_text);
-    h_out.appendChild(btn_out);
-    h_out.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+    btn_out.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
     <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
     <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
     </svg>`;
+    h_user.appendChild(user_text);
+    h_out.appendChild(btn_out);
     hori_bar_info.appendChild(h_user);
     hori_bar_info.appendChild(h_out);
     nav.appendChild(hori_bar_info);
